3034e79a622378b96fd55a8e6d32008cd06bc9ac
Laptop Admin <mictavim@gmail.com>
2024-04-14 14:36:05 +0300

    firs upload project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Column from './Components/Column'
+function App() {
+
+  return (
+    <>
+    
+      <div className='App'>
+        <Column state="PLANNED"/>
+        <Column state="ONGOING"/>
+        <Column state="DONE"/>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { create } from "zustand"
+
+const store = (set) => ({
+    tasks: [{ title: 'TestTask', state: 'DONE' }, { title: 'SECOND', state: 'ONGOING' }],
+    draggedTask: null,
+    addTask: (title, state) =>
+        set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+    deleteTask: (title) =>
+        set((store) => ({ tasks: store.tasks.filter(task=>task.title !== title) })),
+    setDraggedTask: (title)=> set({draggedTask: title}),
+    moveTask: (title,state)=> set((store)=>({tasks: store.tasks.map(task=> task.title === title ? {title,state} : task)}))
+})
+export const useStore = create(store);
